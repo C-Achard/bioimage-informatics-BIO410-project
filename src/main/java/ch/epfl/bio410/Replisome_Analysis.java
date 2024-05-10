a7f9f11e41a5899bbb5b3d661b2219c51355b6d4
Mathilde Morelli <mathilde.morelli@epfl.ch>
2024-05-10 18:33:20 +0200

    denoise DIC func

--- src/main/java/ch/epfl/bio410/Replisome_Analysis.java
+++ src/main/java/ch/epfl/bio410/Replisome_Analysis.java
@@ -1,16 +1,43 @@
 package ch.epfl.bio410;
 
 import ij.IJ;
+import ij.ImagePlus;
+import ij.plugin.ChannelSplitter;
+import ij.process.ImageProcessor;
 import net.imagej.ImageJ;
 import org.scijava.command.Command;
 import org.scijava.plugin.Plugin;
+import ch.epfl.bio410.utils.utils;
 
 
-@Plugin(type = Command.class, menuPath = "Plugins>BII>Project Template")
-public class ProjectCommand implements Command {
+@Plugin(type = Command.class, menuPath = "Plugins>BII>Replisome_Analysis")
+public class Replisome_Analysis implements Command {
 
+	private final double sigma = 10;  // Detection parameters, sigma of the DoG
+
+
+	@Override
 	public void run() {
-		IJ.log("Hello World");
+
+		ImagePlus imp0 = IJ.openImage("C:/Users/mathi/OneDrive/Documents/EPFL/MA4/Bioimage Analysis/Project/Project - SPB - Spot in bacteria/Bacteria/Merged-2.tif");
+		imp0.show();
+		IJ.run("Duplicate...", "duplicate frames=1-10");
+		ImagePlus imp = IJ.getImage();
+		imp.show();
+
+		//changing memory
+		//IJ.run("Memory & Threads...", "maximum=6000 parallel=8 run");
+
+		ImagePlus[] channels = ChannelSplitter.split(imp);
+		System.out.println(channels.getClass());
+		ImagePlus DIC = channels[0];
+		ImagePlus dots = channels[1];
+
+		// Removing noise
+		// call noise removal function
+		ImagePlus denoised = utils.remove_noise(DIC,sigma);
+		denoised.show();
+
 	}
 
 
