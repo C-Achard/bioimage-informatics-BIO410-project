76a5b7fc6a9919e31663f4bf70c0b606bcee3c07
Mathilde Morelli <115156167+matou1604@users.noreply.github.com>
2024-05-11 16:41:57 +0200

    Add denoising func

--- src/main/java/ch/epfl/bio410/utils/utils.java
+++ src/main/java/ch/epfl/bio410/utils/utils.java
@@ -1,1 +1,46 @@
-// TODO : add utils to the package
+package ch.epfl.bio410.utils;
+
+import ij.ImagePlus;
+import ij.plugin.GaussianBlur3D;
+import ij.plugin.ImageCalculator;
+import ij.process.ImageProcessor;
+import ij.IJ;
+
+public class utils {
+
+    public static ImagePlus remove_noise(ImagePlus imp, double sigma) {
+        //imp.show();
+        /* Subtract Background but segmentation actually works best without subtracting background
+            - Background subtraction plugin causes halo in background
+            - Manual BS (using image calculator) causes for black background to be same color as bacteria
+        Integer rolling = 100;
+        IJ.run(imp, "Subtract Background...", "rolling="+rolling+" light sliding stack"); */
+        // Despeckle
+        IJ.run(imp, "Median...", "radius=2 stack");
+        //IJ.run(imp,"Despeckle", "stack"); same but with radius 1
+
+        return imp; //dog(imp, sigma); It seems that dog isn't useful either
+    }
+
+
+    /**
+     * This method performs a difference of Gaussian, removing high-frequency spatial detail (random noise).
+     * Here enhancing edges allows for spot detection later in the detect() function.
+     * Dog works with subtraction a blurred version of the image to a less blurry one (different sigma)
+     *
+     * @param imp contains the pixel data of the image and some basic methods to manipulate it.
+     * @param sigma is the standard deviation of the gaussian we want to apply to the image to blur it
+     * @return the processed image as an ImageProcessor
+     */
+    public static ImagePlus dog(ImagePlus imp, double sigma) {
+        ImagePlus g1 = imp.duplicate(); // Duplicate the input image
+        ImagePlus g2 = g1.duplicate(); // Duplicate the image again
+        double sigma2 = Math.sqrt(2) * sigma;
+        GaussianBlur3D.blur(g1, sigma, sigma, 0);
+        GaussianBlur3D.blur(g2, sigma2, sigma2, 0);
+        ImagePlus dog = ImageCalculator.run(g1, g2, "Subtract create stack");
+        return dog;
+
+    }
+
+}
