a176e7b736ee3b4c593a17a41d95bd432ac18edf
Mathilde Morelli <115156167+matou1604@users.noreply.github.com>
2024-05-11 16:44:29 +0200

    Add segmentation func

--- src/main/java/ch/epfl/bio410/segmentation/segmentation.java
+++ src/main/java/ch/epfl/bio410/segmentation/segmentation.java
@@ -1,1 +1,17 @@
-// TODO : Implement segmentation functions
+package ch.epfl.bio410.segmentation;
+
+import ij.IJ;
+import ij.ImagePlus;
+
+/**
+ * This class implements Segmentation.
+ */
+
+public class segmentation {
+
+    public static void segment(ImagePlus imp) {
+        // Otsu, Default and IsoData work well
+        IJ.run(imp, "Convert to Mask", "method=Otsu background=Dark calculate black");
+    }
+
+}
