69b3fc44607b03b4eb599766fa097fbcb4e95d4c
Mathilde Morelli <mathilde.morelli@epfl.ch>
2024-05-18 20:49:05 +0200

    improvements centroid tracking

--- src/main/java/ch/epfl/bio410/segmentation/segmentation.java
+++ src/main/java/ch/epfl/bio410/segmentation/segmentation.java
@@ -7,6 +7,7 @@ import ij.plugin.frame.RoiManager;
 //import java.lang.Object;
 import ij.measure.ResultsTable;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,15 +16,19 @@ import static ij.IJ.selectWindow;
 import static ij.plugin.frame.RoiManager.multiMeasure;
 import static java.lang.Math.pow;
 import static java.lang.Math.sqrt;
-//import java.lang.Cloneable;
+import java.lang.Cloneable;
 
 
 /**
- * This class implements Segmentation.
+ * This class implements the segmentation of the bacteria
  */
 
 public class segmentation {
 
+    /**
+     * This method segments the bacteria in the image
+     * @param imp the image to segment
+     */
     public static void segment(ImagePlus imp) {
         IJ.run(imp, "Maximum...", "radius=1.5 stack"); // for the watershed to work better
         // Otsu, Default and IsoData work well
@@ -34,53 +39,123 @@ public class segmentation {
         IJ.run(imp, "Watershed", "stack"); //try adjustable watershed instead (installable plugin)
 
         // Add labels:
-        find_centroids(imp);
+        find_centroids(imp); // saves results in csv files
+        List<List<String>> results = read_csv("C:/Users/mathi/OneDrive/Documents/EPFL/MA4/BioimageAnalysis/Project/bioimage-informatics-BIO410-project/DATA/analyze_particle_results/Results.csv");
+        List<List<String>> summary = read_csv("C:/Users/mathi/OneDrive/Documents/EPFL/MA4/BioimageAnalysis/Project/bioimage-informatics-BIO410-project/DATA/analyze_particle_results/Summary.csv");
 
+        // Stuff to check and print out
+        System.out.println(summary.size());
 
+        // find the closest bacteria on next frame by computing euclidian distance between centroid of current and all bacteria on next frame
+        List<List<String>> new_bacteria_labels = results;
+
+        // loop over all frames
+        for (int frame = 0; frame < summary.size(); frame++) {
+            // for each bacteria in current frame, find the closest bacteria in next frame
+            int bact_number_current_frame = get_bact_count(summary, frame);
+            ???get_bacteria_ids_of_frame(frame, results);
+            for (int i = 0; i <= bact_number_current_frame; i++) {
+                int closest_bacteria_label = label_bacteria(i, frame, frame + 1, results);
+                // do something with closest_bacteria_label
+            }
+        }
     }
+
 
     // find centroid data in DATA
     public static void find_centroids(ImagePlus imp) {
-        IJ.run("Set Measurements...", "area centroid limit add redirect=None decimal=3");
+        IJ.run("Set Measurements...", "area centroid stack limit add redirect=None decimal=3");
         IJ.run(imp, "Analyze Particles...", "size=25-Infinity pixel display exclude summarize overlay add composite stack");
 
         IJ.selectWindow("Results");
         IJ.saveAs("Results", "C:/Users/mathi/OneDrive/Documents/EPFL/MA4/BioimageAnalysis/Project/bioimage-informatics-BIO410-project/DATA/analyze_particle_results/Results.csv");
         //IJ.saveAs("Results", "./../../../../../../../DATA/analyze_particle_results/Results.csv");
-        //selectWindow("Results");
-        //IJ.run("Close");
 
         IJ.selectWindow("Summary of C1-Merged-2.tif");
         IJ.saveAs("Results", "C:/Users/mathi/OneDrive/Documents/EPFL/MA4/BioimageAnalysis/Project/bioimage-informatics-BIO410-project/DATA/analyze_particle_results/Summary.csv");
         //IJ.saveAs("Results", "./../../../../../../../DATA/analyze_particle_results/Summary.csv"); // size of bact gets bigger so make adjustable watershed
-
-        List<List<String>> results = read_csv("C:/Users/mathi/OneDrive/Documents/EPFL/MA4/BioimageAnalysis/Project/bioimage-informatics-BIO410-project/DATA/analyze_particle_results/Results.csv");
-        List<List<String>> summary = read_csv("C:/Users/mathi/OneDrive/Documents/EPFL/MA4/BioimageAnalysis/Project/bioimage-informatics-BIO410-project/DATA/analyze_particle_results/Summary.csv");
-
-        double[] centroid = get_centroid(1, results);
-        System.out.print(Arrays.toString(centroid));
-        int bact_number_frame_1 = get_initial_bact_number(summary);
-
+        // TODO: demander à Cyril comment faire pour que les fichiers soient sauvegardés dans le bon dossier (quel est le bon path)
     }
 
 
+    /**
+     * This method calculates the euclidian distance between two centroids
+     * @param centroidA
+     * @param centroidB
+     * @return the euclidian distance between the two centroids
+     */
     public static double euclidian_distance(double[] centroidA, double[] centroidB) {
         return sqrt(pow(centroidA[1] - centroidB[1], 2) + pow(centroidA[2] - centroidB[2], 2));
     }
 
 
-    public static void get_next_bact() {
+    /**
+     * This method labels the bacteria in the next frame
+     * @param bacteria_label
+     * @param frame1
+     * @param frame2
+     * @param results
+     * @return the label of the closest bacteria
+     */
+    public static int label_bacteria(int bacteria_label, int frame1, int frame2, List<List<String>> results){
+        double[] current_centroid = get_centroid(bacteria_label, results);
+        double min_distance = Double.MAX_VALUE;
+        int closest_bacteria_label = -1;
+
+        for (int i = 0; i < get_bact_count(results, frame2); i++) {
+            double[] next_centroid = get_centroid(i, results);
+            double distance = euclidian_distance(current_centroid, next_centroid);
+
+            if (distance < min_distance) {
+                min_distance = distance;
+                closest_bacteria_label = i;
+            }
+        }
+        return closest_bacteria_label;
 
     }
 
+    /** This method iterates over each row in the results list.
+     * If the frame number of the row (which is now the last element of the row) matches the given frame number,
+     * it adds the bacteria ID of that row (which is the first element of the row) to the bacteria_ids list.
+     * The method returns the bacteria_ids list, which contains the IDs of all bacteria in the given frame.
+     * @param frame
+     * @param results
+     * @return the list of bacteria IDs in the given frame
+     */
+    public static List<String> get_bacteria_ids_of_frame(int frame, List<List<String>> results) {
+        List<String> bacteria_ids = new ArrayList<>();
+
+        for (List<String> row : results) {
+            if (Integer.parseInt(row.get(row.size() - 1)) == frame) { // get the last element of the row for the frame number
+                bacteria_ids.add(row.get(0)); // get the first element of the row for the bacteria id
+            }
+        }
+        return bacteria_ids;
+    }
+
+
+    /**
+     * This method returns the centroid of a bacteria
+     * @param bacteria_label
+     * @param results
+     * @return the centroid of the bacteria
+     */
     public static double[] get_centroid(int bacteria_label, List<List<String>> results) {
         double X = Double.parseDouble(results.get(bacteria_label).get(2));
         double Y = Double.parseDouble(results.get(bacteria_label).get(3));
         return new double[] {X,Y};
     }
 
-    public static int get_initial_bact_number(List<List<String>> summary) {
-        int total = Integer.parseInt((summary.get(1).get(1)));
+
+    /**
+     * This method returns the number of bacteria in a frame i
+     * @param summary
+     * @param i the frame number
+     * @return the number of bacteria in the frame
+     */
+    public static int get_bact_count(List<List<String>> summary, int i) {
+        int total = Integer.parseInt((summary.get(i).get(1)));
         return total;
     }
 
